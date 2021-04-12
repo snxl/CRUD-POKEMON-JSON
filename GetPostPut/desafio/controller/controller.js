9d0d6f4ebc797b272d0c135494242b0b506aa3ee
NICOLAS SOARES GOMES <62858129+snxl@users.noreply.github.com>
2021-04-11 23:55:15 -0300

    terminado CRUD JSON

--- GetPostPut/desafio/controller/controller.js
+++ GetPostPut/desafio/controller/controller.js
@@ -10,20 +10,67 @@ const controller = {
     delete: (req, res) => {
         const { id } = req.params;
 
-        let jsonCarregado = fs.readFileSync(jsonPokemon)
+        let readJson = fs.readFileSync(jsonPokemon)
+        let jsonObjeto = JSON.parse(readJson)
 
-        let jsonParaDeletar = JSON.parse(jsonCarregado)
+        jsonObjeto.array.forEach(element => {
+            if(element.id == id){
+                element.name = null, 
+                element.description = null, 
+                element.type = null, 
+                element.healthPoints = null, 
+                element.specialAttack = null, 
+                element.defense = null, 
+                element.attack = null, 
+                element.experience = null, 
+                element.specialDefense = null
+            }
+        })
+        
+        let transformarjson = JSON.stringify(jsonObjeto, null, 2)
 
-        jsonParaDeletar.forEach(element => {
-            
-        });
+        fs.writeFileSync(jsonPokemon, transformarjson)
 
-        res.json(jsonParaDeletar)
+        res.status(200).send("checar com GET")
+        
     },
     put: (req, res) => {
         const { id } = req.params;
 
-        let jsonCarregado = fs.readFileSync(jsonPokemon)
+        let readJson = fs.readFileSync(jsonPokemon)
+        let jsonObjeto = JSON.parse(readJson)
+
+        const { 
+            name, 
+            description, 
+            type, 
+            healthPoints, 
+            specialAttack, 
+            defense, 
+            attack, 
+            experience, 
+            specialDefense
+        } = req.body;
+
+        jsonObjeto.array.forEach(element => {
+            if(element.id == id){
+                element.name = name, 
+                element.description = description, 
+                element.type = type, 
+                element.healthPoints = healthPoints, 
+                element.specialAttack = specialAttack, 
+                element.defense = defense, 
+                element.attack = attack, 
+                element.experience = experience, 
+                element.specialDefense = specialDefense
+            }
+        })
+        
+        let transformarjson = JSON.stringify(jsonObjeto, null, 2)
+
+        fs.writeFileSync(jsonPokemon, transformarjson)
+
+        res.status(200).send("checar com GET")
     },
     post: (req, res) => {
         const { 
--- GetPostPut/desafio/services/LegendariesService.js
+++ GetPostPut/desafio/services/LegendariesService.js
@@ -18,7 +18,7 @@ const LegendariesService = {
         experience, 
         specialDefense
         ) => {
-        const readJson = fs.readFileSync(jsonPokemon)
+        let readJson = fs.readFileSync(jsonPokemon)
             
         const newLegendary = new LegendaryModel(
             id, 
@@ -40,6 +40,48 @@ const LegendariesService = {
         let transformarjson = JSON.stringify(jsonObjeto, null, 2)
 
         fs.writeFileSync(jsonPokemon, transformarjson)
+    },
+    putLegendarie: (objeto) =>{
+        let readJson = fs.readFileSync(jsonPokemon)
+        let jsonObjeto = JSON.parse(readJson)
+
+        const corpo = {...objeto}
+
+        const {
+            res,
+            id,
+            name, 
+            description, 
+            type, 
+            healthPoints, 
+            specialAttack, 
+            defense, 
+            attack, 
+            experience, 
+            specialDefense
+        } = corpo
+
+        let newData = jsonObjeto.array.forEach(element => {
+            if(element.id == id){
+                element.name = name, 
+                element.description = description, 
+                element.type = type, 
+                element.healthPoints = healthPoints, 
+                element.specialAttack = specialAttack, 
+                element.defense = defense, 
+                element.attack = attack, 
+                element.experience = experience, 
+                element.specialDefense = specialDefense
+            }
+
+            let transformarjson = JSON.stringify(jsonObjeto, null, 2)
+
+            fs.writeFileSync(jsonPokemon, transformarjson)
+
+            res.send("checar com get")
+            
+        });
+        
     }
 }
 
